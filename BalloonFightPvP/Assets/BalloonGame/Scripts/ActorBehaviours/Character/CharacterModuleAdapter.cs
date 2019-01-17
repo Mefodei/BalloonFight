006a3591d7b6400050c5c334a2971723d78591ff
Mihaylov Sergey <sergey.mihaylov@zimad.com>
2019-01-17 20:27:10 +0300

    - update module commit

---
+++ BalloonFightPvP/Assets/BalloonGame/Scripts/ActorBehaviours/Character/CharacterModuleAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityTools.UniNodeEditor.Connections;
+
+namespace BalloonGame.Modules.Character
+{
+    [CreateAssetMenu(menuName = "BalloonGame/Modules/CharacterModule",fileName = "CharacterModule")]
+    public class CharacterModuleAdapter : NodeModuleAdapter
+    {
+        
+        private List<string> _characterMessages = new List<string>()
+        {
+            "MoveLeft",
+            "MoveLeft",
+            "MoveLeft",
+            "MoveLeft",
+        };
+        
+        
+        
+    }
+}
